6651d3d69fb5e8ffa31c877213165db198e908c6
UK-17 <utkarsh.kushagra@outlook.com>
2022-04-08 18:05:53 +0530

    removed valid_date func

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,4 +1,3 @@
-from app.parser import date_parser
 from fastapi import APIRouter
 from fastapi.param_functions import Depends
 from app.model import schemas
@@ -13,11 +12,6 @@ logger = logging.getLogger(__name__)
 
 
 router=APIRouter()
-
-@router.post('/get-valid-date')
-async def get_medicine_info(data:schemas.DateUtterance):
-    result = date_parser.get_date(data.utterance)
-    return result
 
 @router.post('/get-datetime-range')
 async def get_medicine_info(data:schemas.DateUtterance):
